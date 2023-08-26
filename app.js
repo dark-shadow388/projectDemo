fe16dc678d33485a9a78421d3a3d48903d451907
Shadow <djones.sdx@gmail.com>
2023-08-26 20:41:26 +0600

    solved

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 // let a = 10;
 // console.log(a);
-const fruits = ['Banana', 'Apple', 'Orange', 'Mango'];
+// const fruits = ['Banana', 'Apple', 'Orange', 'Mango'];
 
-const newFruits = ['kiwi', 'Pineapple'];
-finalFruits = newFruits.concat(fruits);
-console.log(finalFruits);
+// const newFruits = ['kiwi', 'Pineapple'];
+// finalFruits = newFruits.concat(fruits);
+// console.log(finalFruits);
+const points = [30, 40, 100, 1, 5, 25, 10, 200];
+points.sort(function (a, b) {
+  // console.log(a, b, a - b);
+  return a - b;
+});
+console.log(points);
