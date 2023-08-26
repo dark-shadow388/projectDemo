5bbce3cfb921978efb94603149be482594947108
Shadow <djones.sdx@gmail.com>
2023-08-26 20:50:09 +0600

    xz

--- app.js
+++ app.js
@@ -6,8 +6,9 @@
 // finalFruits = newFruits.concat(fruits);
 // console.log(finalFruits);
 const points = [30, 40, 100, 1, 5, 25, 10, 200];
+// points.sort((a, b) => a - b);
 points.sort(function (a, b) {
   // console.log(a, b, a - b);
   return a - b;
 });
-console.log(points);
+//console.log(points);
